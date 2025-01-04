449c00f481c8529f3988ab13aa2c92e282ec2cf6
Aisheair <ankitkargupta98313@gmail.com>
2025-01-04 21:49:42 +0530

    done

--- cProgram/ANDlogicalOperator.c
+++ cProgram/ANDlogicalOperator.c
@@ -1,18 +1,1 @@
-#include <stdio.h>
-#include <stdbool.h>
 
-int main(){
-    // logical operators = && (AND) checks if two conditions are true
-
-    float temp = 24;
-    bool sunny = true;
-
-    if(temp >= 0 && temp <= 30 && sunny){  
-        printf("\nthe weather is good!!");
-    }
-    else{
-        printf("\nthe weather is bad!!");
-    }
-
-    return 0;
-}
---
+++ cProgram/printAnArrayWithLoop.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    2D array = am array, where each element is an entire array
+               useful if you need a matrix, grid, or  table of data
+
+    int numers[2][3] ={
+        {1, 2, 3},
+        {4, 5, 6}
+        };
+
+    int number[2][3]
+
+    return 0;
+}
